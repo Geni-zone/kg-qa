af7e2de7fa65613e0a4ab522111e96a31ff650e2
Tugoph <yunw3@illinois.edu>
2023-08-27 21:29:18 -0500

    Function Signatures Created

---
+++ design.py
@@ -0,0 +1,55 @@
+# Every 4k tokens with 1k overlap
+# don't split sentences
+# could split by '\n\n' or '\n'
+def text_split():
+    pass
+
+
+# Every 4k tokens with 1k overlap
+def entity_extract():
+    pass
+
+
+# gather the excepts about the entity
+# ask GPT
+def predicate_extract():
+    pass
+
+
+def entity_aggregate():
+    pass
+
+
+# use hypernym
+def predicate_aggregate():
+    pass
+
+
+def merge_kg():
+    pass
+
+
+def infer_schema():
+    pass
+
+
+def get_embedding():
+    pass
+
+
+def entity_from_q():
+    pass
+
+
+def predicate_from_q():
+    pass
+
+
+# goes thrugh all possible tail entities
+def traverse_entity():
+    pass
+
+
+# goes through all possible predicates
+def traverse_predicate():
+    pass
