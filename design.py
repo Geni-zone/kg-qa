198aa900cfba203ecec940a001d24aa1eca719fc
Tugoph <yunw3@illinois.edu>
2023-08-27 23:33:20 -0500

    Update design.py

--- design.py
+++ design.py
@@ -1,36 +1,51 @@
 # Every 4k tokens with 1k overlap
 # don't split sentences
 # could split by '\n\n' or '\n'
+# Isaac
 def text_split():
     pass
 
 
 # Every 4k tokens with 1k overlap
+# Tiger
 def entity_extract():
     pass
 
 
 # gather the excepts about the entity
 # ask GPT
+# Phrase Selection & Mention Recognition & Relation Extraction
+# Philip: suppose you have a list of entities.
 def predicate_extract():
     pass
 
 
+# Tiger
 def entity_aggregate():
     pass
 
 
 # use hypernym
+# Isaac
 def predicate_aggregate():
     pass
 
 
+# All
 def merge_kg():
     pass
-
-
 def infer_schema():
     pass
+
+
+
+
+
+
+
+
+
+
 
 
 def get_embedding():
