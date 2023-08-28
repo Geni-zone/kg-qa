2c36f1f421077d821a84356502124292a49ececd
Isaac Zheng <isaaczheng0414@gmail.com>
2023-08-27 23:35:17 -0500

    update split_text

--- design.py
+++ design.py
@@ -1,8 +1,33 @@
+from typing import Union
+
 # Every 4k tokens with 1k overlap
 # don't split sentences
 # could split by '\n\n' or '\n'
-def text_split():
-    pass
+def split_text(input: str, window_size: int=6000, overlap: Union[int, None]=3000):
+    stride = window_size - overlap
+    start = 0
+    end = 0
+    result = list()
+    while start + window_size <= len(input):
+        end = start + window_size
+        while input[end - 1] != '\n' and end > start:
+            end -= 1
+        if end <= start:
+            end = start + window_size
+        result.append(input[start:end])
+        if stride == 0 or stride is None:
+            start = end
+        else:
+            start_tmp = start
+            start += stride
+            while input[start - 1] != '\n' and start > start_tmp:
+                start -= 1
+            if start <= start_tmp:
+                start = start_tmp + stride
+    
+    if start + window_size > len(input):
+        result.append(input[start:len(input)])
+    return result
 
 
 # Every 4k tokens with 1k overlap
